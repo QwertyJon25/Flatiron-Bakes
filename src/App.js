143ee5d7a1f9b1f513a3877cee2ed126208aae3e
Jon Courtier <jonaqwerty@yahoo.com>
2021-09-24 15:07:42 -0400

    push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import Header from "./Header"
+import CakeCard from "./CakeCard";
+import SearchBar from "./SearchBar";
+import cakes from './data';
+import CakeDetail from './CakeDetail'
+
 
 function App() {
+  const [selectedCake, setSelectedCake] = useState(null)
+  const [visible, setVisible] = useState(false)
+  const [cakeList, setCakes] = useState(cakes)
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      {visible?<SearchBar />:null}
+      <button onClick={() => setVisible(!visible)}>{visible?'x':'Form'}</button>
+      <br/>
+        {selectedCake? <CakeDetail cake={selectedCake}/>:null}
+
+      {cakes.map(cake => <CakeCard cake={cake} setSelectedCake={setSelectedCake}/>)}
+    </>
   );
 }
 
